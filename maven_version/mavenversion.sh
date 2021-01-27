d8b6309d54a11e92ee79f81d47a16ae18e926360
Ranabir Chakraborty <59959091+RanabirChakraborty@users.noreply.github.com>
2021-01-27 21:12:15 +0530

    Update mavenversion.sh

--- maven_version/mavenversion.sh
+++ maven_version/mavenversion.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 #author Ranabir Chakraborty
 
-mvn --version > version.txt
-echo $(head -n 1 version.txt) > version1.txt
-var1=$(awk '{for (I=1;I<NF;I++) if ($I == "Maven") print $(I+1)}' version1.txt)
 
-first=${var1%%.*}
-echo $first
-last=${var1##*.}
-echo $last
-mid=${var1##$first.}
-mid=${mid%%.$last}
-echo $mid
-if [[ "$first" -ge "3" && "$mid" -ge "6" && "$last" -ge "0" ]]
-then
-        echo "maven version is new"
+mvnv()
+{
+        mvn --version > version.txt
+        echo $(head -n 1 version.txt) > version1.txt
+        var1=$(awk '{for (I=1;I<NF;I++) if ($I == "Maven") print $(I+1)}' version1.txt)
+
+        first=${var1%%.*}
+        #echo $first
+        last=${var1##*.}
+        #echo $last
+        mid=${var1##$first.}
+        mid=${mid%%.$last}
+        #echo $mid
+        if [[ "$first" -ge "3" && "$mid" -ge "6" && "$last" -ge "0" ]]
+        then
+                echo "new"
+        else
+                echo "old"
+        fi
+}
+
+if [[ man = "new" ]]; then
+        mvn clean install
 else
-        echo "maven version is old"
+        ./mvnw clean install
 fi
-
-rm -rf version.txt version1.txt
